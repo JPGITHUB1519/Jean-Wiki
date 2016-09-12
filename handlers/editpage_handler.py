a5df411d0d744af952be050bebc01423c5525bfb
Jean Ureña <juanpedro1519@gmail.com>
2016-09-12 10:57:08 -0400

    Add edit Function

--- handlers/editpage_handler.py
+++ handlers/editpage_handler.py
@@ -5,8 +5,12 @@ from google.appengine.ext import db
 class EditPageHandler(Handler) :
 	# parameter url is for get in the url
 	def get(self, url) :
-		#self.write(url)
-		self.render("edit_page.html")
+		post = WikiPost.query(ancestor = ancestor_key).filter(WikiPost.url == url).get()
+		# if the post exits load the content of the post and assign it to the textarea
+		if post :
+			self.render("edit_page.html", post_value = post.content)
+		else :
+			self.render("edit_page.html")
 
 	def post(self, url) :
 		# saving wiki post in database
--- handlers/wikipage_handler.py
+++ handlers/wikipage_handler.py
@@ -6,15 +6,18 @@ import time
 class WikiPageHandler(Handler) :
 	# parameter url is for get in the url
 	def get(self, url) :
+		# new version of ndb
+		post = WikiPost.query(ancestor = ancestor_key).filter(WikiPost.url == url).get()
 		# if the user is logged
 		if self.user :
 			# get url
-			# new version of ndb
-			post = WikiPost.query(ancestor = ancestor_key).filter(WikiPost.url == url).get()
 			if post :
-				self.render("wikipage.html", post = post.content)
+				# send url for edit if the user wants
+				# send access for know if the user is logged
+				self.render("wikipage.html", post = post.content, url = post.url, access = True)
 			else :
 				#if the post do not exits redirect to edit_
 				self.redirect('/_edit' + url)
 		else :
-			self.render("wikipage.html")
+			# if the user do not exits only see
+			self.render("wikipage.html", post = post.content, access = False)
